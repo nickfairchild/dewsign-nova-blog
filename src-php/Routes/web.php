87cc2e2942414b197fef1df0415ad07c9840a19b
Nick Fairchild <nick.fairchild@gmail.com>
2024-02-13 17:20:02 +0000

    fix creating article

--- src-php/Routes/web.php
+++ src-php/Routes/web.php
@@ -6,5 +6,5 @@
 Route::middleware('web')->prefix('blog')->name('blog.')->group(function () {
     Route::get('/', 'Dewsign\NovaBlog\Http\Controllers\BlogController@index')->name('index');
     Route::get('{category}', 'Dewsign\NovaBlog\Http\Controllers\BlogController@list')->name('list');
-    Route::get('{category}/{article}', 'Dewsign\NovaBlog\Http\Controllers\BlogController@show')->name('show');
+    Route::get('{category?}/{article?}', 'Dewsign\NovaBlog\Http\Controllers\BlogController@show')->name('show');
 });
